8504fbfeb1b3e982e30050e15be99b6aaab49bc1
vandenkieboom <159015325+vandenkieboom@users.noreply.github.com>
2024-02-14 19:57:08 +0100

    Add files via upload

---
+++ website2/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('profileButton').addEventListener('click', function() {
+  document.getElementById('badge').classList.toggle('bg-danger');
+  document.getElementById('badge').classList.toggle('bg-success');
+});
